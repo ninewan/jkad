3e1d000871c041edf01ed70b51cefed555133924
polaco <polaco@320fa39e-641b-0410-b3c6-25b864144bd5>
2006-08-21 02:59:25 +0000

    Implementacao de interface e maior controle para Jkad

---
+++ Tools/src/jkadgui/JKadGUI.java
@@ -0,0 +1,145 @@
+package jkadgui;
+
+import jKad.controller.JKad;
+import jKad.controller.JKadSystem;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListModel;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+
+public class JKadGUI implements ListDataListener
+{
+    private JFrame frame = null;  //  @jve:decl-index=0:visual-constraint="65,17"
+    private JPanel frameContentPane = null;
+    private JList nodeList = null;
+    private JPanel controlPanel = null;
+    private JPanel viewPanel = null;
+    private JScrollPane listScrollPane = null;
+    private NodeListModel systemList = null;
+    
+    public JKadGUI()
+    {
+        this.systemList = new NodeListModel();
+    }
+    
+    public JKadGUI(List<JKadSystem> systemList)
+    {
+        this();
+        this.systemList.addAll(systemList);
+    }
+    
+    public JKadGUI(JKad jkad)
+    {
+        this(jkad.getSystems());
+    }
+    
+    public void init()
+    {
+        this.getFrame().setVisible(true);
+    }
+    
+    private JFrame getFrame()
+    {
+        if (frame == null)
+        {
+            frame = new JFrame();
+            frame.setTitle("JKad GUI");
+            frame.setSize(new Dimension(500, 400));
+            frame.setContentPane(getFrameContentPane());
+        }
+        return frame;
+    }
+
+    private JPanel getFrameContentPane()
+    {
+        if (frameContentPane == null)
+        {
+            frameContentPane = new JPanel();
+            frameContentPane.setLayout(new BorderLayout());
+            frameContentPane.add(getViewPanel(), BorderLayout.CENTER);
+            frameContentPane.add(getControlPanel(), BorderLayout.SOUTH);
+        }
+        return frameContentPane;
+    }
+
+    private JList getNodeList()
+    {
+        if (nodeList == null)
+        {
+            nodeList = new JList(systemList);
+            systemList.addListDataListener(this);
+        }
+        return nodeList;
+    }
+    
+    public void contentsChanged(ListDataEvent e)
+    {
+        this.getNodeList().update(getNodeList().getGraphics());
+        System.out.println("JKadGUI.contentsChanged()");
+    }
+
+    public void intervalAdded(ListDataEvent e)
+    {
+        this.getNodeList().update(getNodeList().getGraphics());
+        System.out.println("JKadGUI.intervalAdded()");
+    }
+
+    public void intervalRemoved(ListDataEvent e)
+    {
+        this.getNodeList().update(getNodeList().getGraphics());
+        System.out.println("JKadGUI.intervalRemoved()");
+    }
+
+    private JPanel getControlPanel()
+    {
+        if (controlPanel == null)
+        {
+            controlPanel = new JPanel();
+            controlPanel.setLayout(new GridBagLayout());
+        }
+        return controlPanel;
+    }
+
+    private JPanel getViewPanel()
+    {
+        if (viewPanel == null)
+        {
+            viewPanel = new JPanel();
+            viewPanel.setLayout(new BorderLayout());
+            viewPanel.add(getListScrollPane(), BorderLayout.CENTER);
+        }
+        return viewPanel;
+    }
+    
+    private JScrollPane getListScrollPane()
+    {
+        if (listScrollPane == null)
+        {
+            listScrollPane = new JScrollPane();
+            listScrollPane.setViewportView(getNodeList());
+        }
+        return listScrollPane;
+    }
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        JKad jkad = new JKad();
+        Thread jkadThread = new Thread(jkad);
+        jkadThread.start();
+        Thread.sleep(5000);
+        JKadGUI gui = new JKadGUI(jkad);
+        gui.init();
+        
+        while(jkadThread.isAlive())
+            try { jkadThread.join(); } catch (InterruptedException e){ }
+    }
+}
---
+++ Tools/src/jkadgui/NodeListModel.java
@@ -0,0 +1,180 @@
+package jkadgui;
+
+import jKad.controller.JKadSystem;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.ListModel;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+
+public class NodeListModel extends ArrayList<JKadSystem> implements ListModel
+{
+    private List<ListDataListener> listenersList;
+    
+    public NodeListModel()
+    {
+        super();
+        this.listenersList = new ArrayList<ListDataListener>();
+    }
+    
+    public NodeListModel(List<JKadSystem> systemList)
+    {
+        this();
+        this.addAll(systemList);
+    }
+    
+    public void addListDataListener(ListDataListener l)
+    {
+        this.listenersList.add(l);
+    }
+    
+    public Object getElementAt(int index)
+    {
+        return get(index);
+    }
+    
+    public int getSize()
+    {
+        return size();
+    }
+
+    public void removeListDataListener(ListDataListener l)
+    {
+        this.listenersList.remove(l);
+    }
+
+    public void add(int index, JKadSystem element)
+    {
+        super.add(index, element);
+        this.notifyListeners(ListDataEvent.INTERVAL_ADDED, index, index);
+    }
+
+    @Override
+    public boolean add(JKadSystem o)
+    {
+        int sizeBefore = size();
+        boolean added = super.add(o);
+        if(added)
+            this.notifyListeners(ListDataEvent.INTERVAL_ADDED, sizeBefore, size());
+        return added;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends JKadSystem> c)
+    {
+        int sizeBefore = size();
+        boolean added = super.addAll(c);
+        if(added)
+            this.notifyListeners(ListDataEvent.INTERVAL_ADDED, sizeBefore, size());
+        return added;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends JKadSystem> c)
+    {
+        int sizeBefore = size();
+        boolean added = super.addAll(index, c);
+        if(added)
+            this.notifyListeners(ListDataEvent.INTERVAL_ADDED, index, index + (size() - sizeBefore));
+        return added;
+    }
+
+    @Override
+    public void clear()
+    {
+        int sizeBefore = size();
+        super.clear();
+        this.notifyListeners(ListDataEvent.INTERVAL_REMOVED, 0, sizeBefore);
+    }
+
+    @Override
+    public JKadSystem remove(int index)
+    {
+        JKadSystem removed = super.remove(index);
+        this.notifyListeners(ListDataEvent.INTERVAL_REMOVED, index, index);
+        return removed;
+    }
+
+    @Override
+    public boolean remove(Object o)
+    {
+        int index = indexOf(o);
+        boolean removed = super.remove(o);
+        if(removed)
+            this.notifyListeners(ListDataEvent.INTERVAL_REMOVED, index, index);
+        return removed;
+    }
+
+    @Override
+    public JKadSystem set(int index, JKadSystem element)
+    {
+        JKadSystem replaced = super.set(index, element);
+        this.notifyListeners(ListDataEvent.CONTENTS_CHANGED, index, index);
+        return replaced;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c)
+    {
+        boolean removed = false;
+        for (Iterator<JKadSystem> iterator = iterator(); iterator.hasNext();) 
+        {
+            JKadSystem system = iterator.next();
+            if (c.contains(system)) 
+            {
+                int index = indexOf(system);
+                iterator.remove();
+                notifyListeners(ListDataEvent.INTERVAL_REMOVED, index, index);
+                removed = true;
+            }
+        }
+        return removed;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c)
+    {
+        boolean removed = false;
+        for (Iterator<JKadSystem> iterator = iterator(); iterator.hasNext();) 
+        {
+            JKadSystem system = iterator.next();
+            if (!c.contains(system)) 
+            {
+                int index = indexOf(system);
+                iterator.remove();
+                notifyListeners(ListDataEvent.INTERVAL_REMOVED, index, index);
+                removed = true;
+            }
+        }
+        return removed;
+    }
+    
+    private void notifyListeners(int type, int index0, int index1)
+    {
+        switch(type)
+        {
+            case ListDataEvent.CONTENTS_CHANGED:
+            {
+                for (ListDataListener listener : listenersList)
+                    listener.contentsChanged(new ListDataEvent(this, ListDataEvent.CONTENTS_CHANGED, index0, index1));
+                break;
+            }
+            case ListDataEvent.INTERVAL_ADDED:
+            {
+                for (ListDataListener listener : listenersList)
+                    listener.intervalAdded(new ListDataEvent(this, ListDataEvent.INTERVAL_ADDED, index0, index1));
+                break;
+            }
+            case ListDataEvent.INTERVAL_REMOVED:
+            {
+                for (ListDataListener listener : listenersList)
+                    listener.intervalRemoved(new ListDataEvent(this, ListDataEvent.INTERVAL_REMOVED, index0, index1));
+                break;
+            }
+        }
+    }
+}
