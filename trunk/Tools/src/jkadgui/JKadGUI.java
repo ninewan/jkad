e3fb164b83784219bc52d84995948cc0a591a748
polaco <polaco@320fa39e-641b-0410-b3c6-25b864144bd5>
2006-09-05 01:41:37 +0000

    Organize Imports

--- trunk/Tools/src/jkadgui/JKadGUI.java
+++ trunk/Tools/src/jkadgui/JKadGUI.java
@@ -16,7 +16,6 @@ import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.ListModel;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
 
